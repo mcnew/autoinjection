e13bd45da84298af2a6e1ca0002449e38b57c3f7
Cesar Martinez <mc_new@yahoo.com>
2020-05-12 23:46:36 -0500

    quitando paquete sin uso com.github.mc_new.web0

--- proy0/web0/src/main/java/com/github/mc_new/web0/config/BaseConfig.java
+++ proy0/web0/src/main/java/com/github/mc_new/web0/config/BaseConfig.java
@@ -4,8 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = { "com.github.mc_new.web0", "com.github.mc_new.jar0", "com.github.mc_new.jar1",
-		"com.github.mc_new.jar2" })
+@ComponentScan(basePackages = { "com.github.mc_new.jar0", "com.github.mc_new.jar1", "com.github.mc_new.jar2" })
 public class BaseConfig {
 
 }
