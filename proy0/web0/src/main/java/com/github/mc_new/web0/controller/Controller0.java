a9ce635f3c0b2ce975e95073738acf6f4de93bae
Cesar Martinez <mc_new@yahoo.com>
2020-05-14 21:28:04 -0500

    mostrando el nombre de la clase inyectada

--- proy0/web0/src/main/java/com/github/mc_new/web0/controller/Controller0.java
+++ proy0/web0/src/main/java/com/github/mc_new/web0/controller/Controller0.java
@@ -1,6 +1,7 @@
 package com.github.mc_new.web0.controller;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,11 +14,14 @@ import com.github.mc_new.jar0.service.DatatypeService;
 @RestController
 public class Controller0 {
 
+	private static final Logger LOGGER = Logger.getLogger(Controller0.class.getName());
+
 	@Autowired
 	private DatatypeService service;
 
 	@GetMapping(path = "/controller")
 	public ResponseEntity<List<Datatype>> list() {
+		LOGGER.info(service.getClass().getName());
 		return ResponseEntity.ok(service.getList());
 	}
 
